bc4918974f396b9fffd2b5ef9a8350a407bb57b2
harshpathakzz <harshpathak2017@gmail.com>
2023-08-23 22:17:11 +0530

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,20 @@
 import { useState } from "react";
 import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
 import { prism } from "react-syntax-highlighter/dist/esm/styles/prism";
+import "./App.css";
 
 export default function App() {
-  const [url, setUrl] = useState("https://randomuser.me/api");
-  const [method, setMethod] = useState("GET");
+  const [url, setUrl] = useState("http://localhost:8080/api/v1/users/login");
+  const [method, setMethod] = useState("POST");
   const [headers, setHeaders] = useState("{}");
   const [parameters, setParameters] = useState("");
   const [authToken, setAuthToken] = useState("");
   const [response, setResponse] = useState("");
   const [contentType, setContentType] = useState("application/json");
+  const [requestBody, setRequestBody] = useState(`{
+  "password": "test@123",
+  "username": "doejohn"
+}`);
 
   const sendRequest = async () => {
     try {
@@ -21,6 +26,10 @@ export default function App() {
           "Content-Type": contentType,
         },
       };
+
+      if (method !== "GET" && method !== "HEAD") {
+        options.body = requestBody;
+      }
 
       const apiUrl = new URL(url);
       if (parameters) {
@@ -37,87 +46,65 @@ export default function App() {
     }
   };
 
-  const inputStyle = {
-    padding: "8px",
-    marginBottom: "10px",
-    width: "300px",
-  };
-
-  const buttonStyle = {
-    padding: "8px 16px",
-    backgroundColor: "#007bff",
-    color: "#fff",
-    border: "none",
-    cursor: "pointer",
-    marginRight: "10px",
-  };
-
-  const responseStyle = {
-    padding: "10px",
-    border: "1px solid #ddd",
-    borderRadius: "5px",
-    fontSize: "14px",
-    textAlign: "left",
-    whiteSpace: "pre-wrap",
-  };
-
   return (
-    <div style={{ textAlign: "center", margin: "20px" }}>
-      <h1>API Testing Prototype</h1>
-      <input
-        type="text"
-        placeholder="Enter API URL"
-        value={url}
-        onChange={(e) => setUrl(e.target.value)}
-        style={inputStyle}
-      />
-      <br />
-      <select
-        value={method}
-        onChange={(e) => setMethod(e.target.value)}
-        style={inputStyle}
-      >
-        <option value="GET">GET</option>
-        <option value="POST">POST</option>
-        <option value="PUT">PUT</option>
-        <option value="DELETE">DELETE</option>
-      </select>
-      <br />
+    <div className="container">
+      <h1 className="title">API Testing Prototype</h1>
+      <div className="input-container">
+        <input
+          type="text"
+          placeholder="Enter API URL"
+          value={url}
+          onChange={(e) => setUrl(e.target.value)}
+          className="input"
+        />
+        <select
+          value={method}
+          onChange={(e) => setMethod(e.target.value)}
+          className="select"
+        >
+          <option value="GET">GET</option>
+          <option value="POST">POST</option>
+          <option value="PUT">PUT</option>
+          <option value="DELETE">DELETE</option>
+        </select>
+      </div>
       <textarea
         placeholder="Enter Headers (JSON format)"
         value={headers}
         onChange={(e) => setHeaders(e.target.value)}
-        style={inputStyle}
+        className="textarea"
       />
-      <br />
       <textarea
         placeholder="Enter Parameters (Key=Value format)"
         value={parameters}
         onChange={(e) => setParameters(e.target.value)}
-        style={inputStyle}
+        className="textarea"
       />
-      <br />
       <input
         type="text"
         placeholder="Enter Authentication Token/API Key"
         value={authToken}
         onChange={(e) => setAuthToken(e.target.value)}
-        style={inputStyle}
+        className="input"
       />
-      <br />
       <select
         value={contentType}
         onChange={(e) => setContentType(e.target.value)}
-        style={inputStyle}
+        className="select"
       >
         <option value="application/json">JSON</option>
         <option value="application/x-www-form-urlencoded">Form Data</option>
       </select>
-      <br />
-      <button onClick={sendRequest} style={buttonStyle}>
+      <textarea
+        placeholder="Enter Request Body (JSON format)"
+        value={requestBody}
+        onChange={(e) => setRequestBody(e.target.value)}
+        className="textarea"
+      />
+      <button onClick={sendRequest} className="send-button">
         Send Request
       </button>
-      <pre style={responseStyle}>
+      <pre className="response">
         <SyntaxHighlighter language="json" style={prism}>
           {response}
         </SyntaxHighlighter>
